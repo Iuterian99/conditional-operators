ee1d03944b7a1d7ce816587744026f126426b5c3
Abduhalim Orzikulov <Abduhalim.orziqulov@bk.ru>
2021-01-27 12:21:04 +0500

    initial commit

---
+++ 5-dars_shartli_operatorlar.py
@@ -0,0 +1,38 @@
+                          #Taqqoslash operatorlari
+
+"""
+a>b "a" -> katta "b" dan
+a<b "a" -> kichik"b" dan
+a == b; -> "a" "b"ga teng
+a != b "a" "b"ga teng emas
+a >= b "a" katta yoki teng "b"ga
+a <= b "a" kichik yoki teng "b"ga
+"""
+print(10 == 10)
+print(12 != 12)
+                          # If, else statement
+
+# name = input("Please enter your name: ")
+# if name == " Abdukhalim ":
+#     print("Awesome name!")
+# else :
+#     print("normal name!")
+
+# Number = input("Please enter a number: ")
+# Convert = int(Number)
+# if Convert % 2 == 0:
+#     print("Number is even!")
+# else:
+#     print("Number is odd!")
+
+                            # Triangle
+
+a = int(input("please enter first side of triangle: "))
+b= int(input("please enter second side of  triangle: "))
+c = int(input("please enter third side of triangle: "))
+if a == b and b==c: # if statementda ":" belgi oxirida qoldiriladi
+    print("teng tomonli uchburchak!")
+elif a != b and a!= c: #"elif" belgisi variantlarini chiqarib beradi
+    print("Teng tomonli bo'lmagan uchburchak!")
+else:
+    print("teng yonli uchburchak!")
---
+++ Uy_ishi_5.py
@@ -0,0 +1,33 @@
+                        #3-Uyga vazifa
+# import math
+# note1 = "Let`s define the number of solutions of quadratic equation 'ax^2 + bx + c = 0'!"
+# note2 = "\nFor this, you have to enter a, b, c values!"
+# print(note1, note2)
+# a = int(input("Please enter the value of 'a': "))
+# b = int(input("Please enter the value of 'b': "))
+# c = int(input("Please enter the value of 'c': "))
+# d = pow(b,2)- 4*a*c
+# if d > 0:
+#     print("Equation has Two unique solutions!")
+# elif d < 0:
+#     print("Equation has no solution!")
+# else:
+#     print("Equation has one solution!")
+
+
+# a = int(input("weight: "))
+# b = input("(L)bs or (K)g : ")
+# if b.upper() == "L":
+#     converted = 0.45 * a
+#     print(f"you are {converted} kilos! ")
+# else: converted = a / 0.45
+# print(f"you are {converted} pounds!")
+inro = "Welcome to Guessing game!"
+second ="\n you can guess max three times! "
+input = "\n please enter numbers from 1 to 5! "
+print(inro, second, input)
+
+name = input("enter")
+print(name)
+#
+
